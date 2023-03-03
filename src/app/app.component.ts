f032351c1b84a71c34fc42e3749e750b48c71eb7
username <thanujathanuja18@yahoo.com>
2023-03-03 17:36:07 +0000

    adding date format in the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name!: string;
+  date!: string;
 
 
   onNameChange(value: string) {
     this.name = value;
   }
+onDateChange(value: string) {
+  this.date = value;
+}
+
 
 
 }
