400d4828347c51edaee72063fd29f2973b8ceed9
username <thanujathanuja18@yahoo.com>
2023-03-04 10:57:38 +0000

    making changes in currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   name!: string;
   date!: string;
-
+  amount!: number;
 
   onNameChange(value: string) {
     this.name = value;
   }
-onDateChange(value: string) {
-  this.date = value;
-}
+  onDateChange(value: string) {
+    this.date = value;
+  }
 
-
-
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
